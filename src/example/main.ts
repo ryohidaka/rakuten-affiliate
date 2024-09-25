3d388232087ab7ed4db711d63654895c8bfdd57b
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-09-25 13:07:06 +0900

    feat: RakutenAffiliate クラスを追加

--- src/example/main.ts
+++ src/example/main.ts
@@ -1,4 +1,9 @@
+import { RakutenAffiliate } from "..";
 import "./style.css";
+
+// RakutenAffiliateクラスのインスタンスを生成
+const client = new RakutenAffiliate();
+console.log(client);
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div></div>
